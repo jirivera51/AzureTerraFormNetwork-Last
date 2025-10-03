692263f5327a9bb3e0f0c50bafc17fb6c6edf463
Jose Rivera <jirivera51@gmail.com>
2025-10-03 11:30:26 -0400

    Initial Terraform config for Azure VNet

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1 - Resource Group
+resource "azurerm_resource_group" "demo1" {
+  name     = "demo1"
+  location = "East US"
+}
+
+# 2 - Virtual Network
+resource "azurerm_virtual_network" "vnetdemo1" {
+  name                = "vnetdemo1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.demo1.location
+  resource_group_name = azurerm_resource_group.demo1.name
+}
+
+# 3 - Subnet
+resource "azurerm_subnet" "subnetdemo1" {
+  name                 = "subnetdemo1"
+  resource_group_name  = azurerm_resource_group.demo1.name
+  virtual_network_name = azurerm_virtual_network.vnetdemo1.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
